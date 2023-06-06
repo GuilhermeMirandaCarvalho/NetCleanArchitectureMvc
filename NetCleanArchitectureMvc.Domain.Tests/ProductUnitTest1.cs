1fef6ba188dc278c32700c33a34abac218e738eb
GuilhermeMirandaCarvalho <guilhermeinfo1@gmail.com>
2023-06-06 08:59:09 -0300

    Testes de Unidade - Produto
    Testes de Unidade - Produto

---
+++ NetCleanArchitectureMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using NetCleanArchitectureMvc.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace NetCleanArchitectureMvc.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
+                99, "product image");
+            action.Should()
+                .NotThrow<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m,
+                99, "product image");
+
+            action.Should().Throw<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Id Invalido, Id esta menor que zero");
+        }
+
+        [Fact]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99,
+                "product image");
+            action.Should().Throw<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>()
+                 .WithMessage("Preencha o Nome com mais de 3 caracteres");
+        }
+
+        [Fact]
+        public void CreateProduct_LongImageName_DomainExceptionLongImageName()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
+                99, "product image toooooooooooooooooooooooooooooooooooooooooooo loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
+
+            action.Should()
+                .Throw<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>()
+                 .WithMessage("Nome imagem não pode ter mais que 250 caracteres");
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullImageName_NoNullReferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<NullReferenceException>();
+        }
+
+        [Fact]
+        public void CreateProduct_WithEmptyImageName_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
+            action.Should().NotThrow<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_InvalidPriceValue_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -9.99m,
+                99, "");
+            action.Should().Throw<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>()
+                 .WithMessage("Preço deve ser maior que zero");
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value)
+        {
+            Action action = () => new Product(1, "Pro", "Product Description", 9.99m, value,
+                "product image");
+            action.Should().Throw<NetCleanArchitectureMvc.Domain.Validation.DomainExceptionValidation>()
+                   .WithMessage("Estoque deve ser maior que zero");
+        }
+
+    }
+}
