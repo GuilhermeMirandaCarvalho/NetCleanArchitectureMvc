e5a1d75ee5062c08f9d57ec37752190becf39c90
GuilhermeMirandaCarvalho <guilhermeinfo1@gmail.com>
2023-06-20 09:24:34 -0300

    CQRS - Command Query Responsability Separation
     CQRS - Implementando os Commands e as Queries para Product

---
+++ NetCleanArchitectureMvc.Application/Products/Commands/ProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using NetCleanArchitectureMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCleanArchitectureMvc.Application.Products.Commands
+{
+    public abstract class ProductCommand: IRequest<Product>
+    {
+        public string Name { get; private set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
